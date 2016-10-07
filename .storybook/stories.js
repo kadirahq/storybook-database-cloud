9da08e14e24523389741eb44a2954e6ddf271c30
Muhammed Thanish <mnmtanish@gmail.com>
2016-10-07 11:52:56 +0530

    Automatically detect app by git origin

--- .storybook/stories.js
+++ .storybook/stories.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import { storiesOf } from '@kadira/storybook'
 
-storiesOf('Hello', module)
-  .add('World', () => (
-    <pre>Hello World</pre>
-  ))
-  .add('Earth', () => (
-    <pre>Hello Earth</pre>
-  ))
+storiesOf('Debug', module).add('Environment', () => (
+  <pre>
+    {'Environment Variables:\n'}
+    {'\nSTORYBOOK_CLOUD_SERVER:\n' + process.env.STORYBOOK_CLOUD_SERVER + '\n'}
+    {'\nSTORYBOOK_CLOUD_APPID:\n' + process.env.STORYBOOK_CLOUD_APPID + '\n'}
+    {'\nSTORYBOOK_CLOUD_DATABASE:\n' + process.env.STORYBOOK_CLOUD_DATABASE + '\n'}
+    {'\nSTORYBOOK_GIT_ORIGIN:\n' + process.env.STORYBOOK_GIT_ORIGIN + '\n'}
+    {'\nSTORYBOOK_GIT_BRANCH:\n' + process.env.STORYBOOK_GIT_BRANCH + '\n'}
+  </pre>
+))
--- src/manager/index.js
+++ src/manager/index.js
@@ -5,10 +5,17 @@ import Database from './database';
 
 // NOTE Storybooks uses the define plugin therefore environment variable
 // will work only when used in the `process.env.STORYBOOK_VALUE` format.
+// NOTE For this databae addon to work, both `STORYBOOK_CLOUD_APPID` and
+// `STORYBOOK_CLOUD_DATABASE` environment variables should be available or
+// both `STORYBOOK_CLOUD_ORIGIN` and `STORYBOOK_CLOUD_BRANCH` environment
+// variables should be available.
+const DEFAULT_SERVER_URL = 'https://hub-backend.getstorybook.io/graphql';
 const DEFAULT_OPTIONS = {
-  url: process.env.STORYBOOK_CLOUD_URL,
+  server: process.env.STORYBOOK_CLOUD_SERVER || DEFAULT_SERVER_URL,
   appId: process.env.STORYBOOK_CLOUD_APPID,
   database: process.env.STORYBOOK_CLOUD_DATABASE,
+  origin: process.env.STORYBOOK_GIT_ORIGIN,
+  branch: process.env.STORYBOOK_GIT_BRANCH,
 };
 
 export default function createDatabase(customOptions = {}) {
